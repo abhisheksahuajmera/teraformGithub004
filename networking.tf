9edfb2f0066e8f263e8c189dd1fb7773cfaff084
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-22 17:02:17 +0530

    Lets create vn sec group

--- networking.tf
+++ networking.tf
@@ -19,3 +19,27 @@ resource "azurerm_subnet" "rg103vn100sn100tg" {
       azurerm_virtual_network.rg103vn100tg
     ]
 }
+
+
+
+resource "azurerm_network_security_group" "rg103vn100sg100tg" {
+  name                = "rg103vn100sg100"
+  location            = local.location 
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+depends_on = [
+    azurerm_virtual_network.rg103vn100tg
+  ]
+}
