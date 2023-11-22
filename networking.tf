0a6d82b3cd93ef1576b686c05715054695bad720
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-22 16:58:21 +0530

    Refactored the subnet section 101

--- networking.tf
+++ networking.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "rg103vn100sn100tg" {
     name                 = "rg103vn100sn100"
     resource_group_name  = local.resource_group_name
     virtual_network_name = local.virtual_network.name
-    address_prefixes     = ["10.0.0.0/24"]
+    address_prefixes     = ["10.0.2.0/24"]
     depends_on = [
       azurerm_virtual_network.rg103vn100
     ]
