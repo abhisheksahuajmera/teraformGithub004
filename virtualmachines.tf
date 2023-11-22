65506d1a5db16a851dc00d9d257bd89c633a2f8a
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-22 18:38:39 +0530

    Resolved 103

--- virtualmachines.tf
+++ virtualmachines.tf
@@ -42,3 +42,50 @@ resource "azurerm_windows_virtual_machine" "r103100100100tg" {  //start witgh ch
     azurerm_network_interface.rg103vn100ni100tg
   ]
 }
+//--
+resource "azurerm_network_interface" "rg103vn100ni101tg" {
+  for_each = toset(local.function)
+  name                = "${each.key}-interface"
+  location            = local.location  
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.rg103vn100sn100tg.id
+    private_ip_address_allocation = "Dynamic"    
+  }
+
+  depends_on = [
+    azurerm_virtual_network.rg103vn100tg
+  ]
+}
+
+
+resource "azurerm_windows_virtual_machine" "r103100100101tg" {
+  for_each = toset(local.function)
+  name                = "${each.key}r103100100101tg"
+  resource_group_name = local.resource_group_name
+  location            = local.location 
+  size                = "Standard_D2s_v3"
+  admin_username      = "r103100100100101"
+  admin_password      = "Azure@123"      
+    network_interface_ids = [
+    azurerm_network_interface.rg103vn100ni101tg[each.key].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_virtual_network.rg103vn100tg,
+    azurerm_network_interface.rg103vn100ni101tg
+  ]
+}
